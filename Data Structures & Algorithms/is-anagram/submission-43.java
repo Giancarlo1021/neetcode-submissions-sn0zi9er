93aad015fac444c2688a99b52ea50f4133e52e48
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2026-04-02 13:53:23 -0700

    Add: is-anagram - submission-43

---
+++ Data Structures & Algorithms/is-anagram/submission-43.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()  != t.length()) return false;
+        int[] key = new int[26];
+        
+        for (int i = 0; i < s.length(); i++) {
+            key[s.charAt(i) - 'a']++;
+            key[t.charAt(i) - 'a']--;
+        }
+        
+        for (int i : key) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+}
