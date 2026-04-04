e5b86480fabd8a91ac469daedc0dcc2bc28afd09
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2026-04-04 07:55:07 -0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> openToClose = Map.of(
+            ')','(',
+            ']','[',
+            '}','{'
+        );
+
+        for (char c : s.toCharArray()) {
+             if (openToClose.containsKey(c)) {
+                if (
+                    stack.size() > 0 &&
+                    stack.peek() == openToClose.get(c)
+                ) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+             } else {
+                stack.push(c);
+             }
+        }
+        return stack.size() == 0;
+    }
+}
