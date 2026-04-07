727b4afee8e5718e4bc73d4f29283c5eeee86b95
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2026-04-06 17:03:37 -0700

    Add: is-anagram - submission-45

---
+++ Data Structures & Algorithms/is-anagram/submission-45.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        key = [0] * 26
+
+        for i in range(len(s)):
+            key[ord(s[i]) - ord('a')]+=1
+            key[ord(t[i]) - ord('a')]-=1
+        
+        return not any(key)
