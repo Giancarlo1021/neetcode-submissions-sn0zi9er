68057df09daf17e688535b879c7f9d32a22211a8
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2026-04-16 14:14:56 -0700

    Add: is-palindrome - submission-23

---
+++ Data Structures & Algorithms/is-palindrome/submission-23.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let l = 0 
+        let r = s.length-1
+
+        while (l < r ) {
+            while (l < r && !isAlpha(s[l])) {
+                l++
+            }
+            while (l < r && !isAlpha(s[r])) {
+                r--
+            }
+
+            if (s[l].toLowerCase() != s[r].toLowerCase()) return false
+            l++
+            r--
+        }
+        return true
+    }
+}
+
+
+    isAlpha = (c) => {
+        return (
+            "A" <= c && c <= "Z" || 
+            "a" <= c && c <= "z" || 
+            "0" <= c && c <= "9"  
+        )
+    }
