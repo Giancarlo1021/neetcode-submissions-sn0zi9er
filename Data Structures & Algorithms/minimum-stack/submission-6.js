3d3c66e3ac4ca3eae4e9c52ee015fd0e8099f50c
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2026-04-17 10:41:29 -0700

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.js
@@ -0,0 +1,42 @@
+class MinStack {
+    constructor() {
+        this.stack = []
+        this.minStack = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        console.log("val", val)
+        this.stack.push(val)
+        val = Math.min(val, this.minStack.length ? this.minStack[this.minStack.length-1] : val)
+        this.minStack.push(val)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        console.log("pop")
+        this.stack.pop()
+        this.minStack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        console.log("top")
+        return this.stack[this.stack.length-1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        console.log("getMin")
+        return this.minStack[this.minStack.length-1]
+    }
+}
