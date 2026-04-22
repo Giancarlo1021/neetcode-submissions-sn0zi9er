78954f7a1e2a94f76965df7eb7e2daf9681c1167
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2026-04-22 06:47:55 -0700

    Add: three-integer-sum - submission-19

---
+++ Data Structures & Algorithms/three-integer-sum/submission-19.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let res = [];
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length; i++) {
+            let a = nums[i];
+
+            if (a > 0) break;
+
+            if (i > 0 && a === nums[i - 1]) continue;
+
+            let l = i + 1;
+            let r = nums.length - 1;
+
+            while (l < r) {
+                let currSum = a + nums[l] + nums[r]
+
+                if (currSum < 0) {
+                    l++
+                } else if (currSum > 0) {
+                    r--
+                } else {
+                    res.push([a, nums[l], nums[r]])
+                    l++
+                    r--
+                    
+                    while(l < r && nums[l] === nums[l-1]) l++
+                }
+            }
+        }
+        return res
+    }
+}
