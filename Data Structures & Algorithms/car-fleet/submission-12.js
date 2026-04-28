7d45b91f27e405113c845c856c2577a0e6357738
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2026-04-28 16:16:07 -0700

    Add: car-fleet - submission-12

---
+++ Data Structures & Algorithms/car-fleet/submission-12.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const pairs = position.map((p,i) => [p, speed[i]]);
+        pairs.sort((a,b) => b[0] - a[0]);
+        let res = 1;
+        let old = (target-pairs[0][0]) / pairs[0][1];
+
+        for (let i = 1; i < pairs.length; i++) {
+            const cur = (target-pairs[i][0]) / pairs[i][1];
+
+            if (old < cur) {
+                res++
+                old = cur;
+            }
+        }
+        return res;
+    }
+}
