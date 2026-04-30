1b0feea86b679da00e7cd4da8acd025eca8f7337
Giancarlo Avila <54759063+Giancarlo1021@users.noreply.github.com>
2026-04-30 07:17:11 -0700

    Add: valid-sudoku - submission-35

---
+++ Data Structures & Algorithms/valid-sudoku/submission-35.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        const rows = Array.from({length: 9}, () => new Set());
+        const cols = Array.from({length: 9}, () => new Set());
+        const squares = Array.from({length: 9}, () => new Set());
+
+        for (let r = 0; r < 9; r++) {
+            for (let c = 0; c < 9; c++) {
+                const b = board[r][c];
+                if (b === '.') continue;
+
+                const sKey = Math.floor(r/3) * 3 + Math.floor(c/3);
+
+                if (
+                    rows[r].has(b) || 
+                    cols[c].has(b) || 
+                    squares[sKey].has(b) 
+                ) return false;
+
+                rows[r].add(b);
+                cols[c].add(b);
+                squares[sKey].add(b);
+            }
+        }
+        return true;
+    }
+}
